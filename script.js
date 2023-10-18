d78353938591e77720f38d85b72874f8c81284df
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2023-10-18 16:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function threeSum(arr, target) {
 // write your code here
-	let initalDiff=Infinity,ans=-1;
+	let initialDiff=Infinity,ans=-1;
 	let n=arr.length;
 
 	for(let i=0;i<=n-3;i++)
@@ -11,13 +11,14 @@ function threeSum(arr, target) {
 								{
 									let sum=arr[i]+arr[j]+arr[k];
 									let diff=Math.abs(sum-target);
-									if(initalDiff>diff){
+									if(initialDiff>diff){
 										ans=sum;
 									}
 								}
 					}
 		}
-  console.log(ans);
+	 console.log(ans);
+ 
 }
 
 module.exports = threeSum;
